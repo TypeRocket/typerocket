15921845f5bbd26395ff0aaf8ebd91e793372c8c
kevindees <kevin@robojuice.com>
2015-01-10 15:49:01 -0500

    complete matrix with all fields we can get

--- plugins/matrix/js.js
+++ plugins/matrix/js.js
@@ -21,6 +21,46 @@ jQuery(document).ready(function($) {
                     data = $('<div class="matrix-field-group tr-repeater-group">' + data + '</div></div>');
 
                     data.prependTo($fields).hide().delay(10).slideDown(300).scrollTop('100%');
+
+
+                    if( $.isFunction($.fn.sortable) ) {
+                        var $sortables = $fields.find('.tr-gallery-list'),
+                            $items_list = $fields.find('.tr-items-list'),
+                            $repeater_fields = $fields.find('.tr-repeater-fields');
+                        if($sortables.length > 0) {
+                            $sortables.sortable();
+                        }
+
+                        if($repeater_fields.length > 0) {
+                            $repeater_fields.sortable({
+                                connectWith: ".tr-repeater-group",
+                                handle: ".repeater-controls"
+                            });
+                        }
+
+                        if($items_list.length > 0) {
+                            $items_list.sortable({
+                                connectWith: ".item",
+                                handle: ".move"
+                            });
+                        }
+
+                    }
+
+                    if( $.isFunction($.fn.datepicker) ) {
+                        $fields.find('.date-picker').each(function(){
+                            $(this).datepicker();
+                        });
+                    }
+
+                    if( $.isFunction($.fn.wpColorPicker) ) {
+                        $fields.find('.color-picker').each(function(){
+                            var pal = $(this).attr('id') + '_color_palette',
+                                settings = { palettes: window[pal] };
+                            $(this).wpColorPicker(settings);
+                        });
+                    }
+
                 },
                 dataType: 'html',
                 complete: function() {
