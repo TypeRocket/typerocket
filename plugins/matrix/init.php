fdae00bd8496ebf0d40d1787c14df539b0f8e0a3
kevindees <kevin@robojuice.com>
2015-01-11 21:11:26 -0500

    add debug mode help to Matrix and small bug fixes

--- plugins/matrix/init.php
+++ plugins/matrix/init.php
@@ -14,6 +14,7 @@ class tr_matrix {
 		$this->include_files = $include_files;
 		$this->settings = $settings;
 		$this->label = $label;
+		$this->mxid = md5(microtime(true)); // set id for matrix random
 
 		wp_enqueue_script( 'typerocket-booyah', tr::$paths['urls']['assets'] . '/js/booyah.js', array('jquery'), '1.0', true );
 		wp_enqueue_script('jquery-ui-sortable', array( 'jquery' ), '1.0', true);
@@ -27,34 +28,70 @@ class tr_matrix {
 		wp_enqueue_script('tr_matrix', tr::$paths['urls']['plugins'] . '/matrix/js.js', array( 'jquery' ), true);
 		wp_localize_script('tr_matrix', 'tr_matrix_url', tr::$paths['urls']['plugins'] . '/matrix');
 		wp_localize_script('tr_matrix', 'tr_matrix_form_group', $form->group);
-		wp_enqueue_style( 'matrix-style', tr::$paths['urls']['plugins'] . '/matrix/css.css' );
 
 	}
 
 	function add() {
 
 		// setup select list of files
-		$dir = __DIR__ . '/' . $this->name;
-		$files = scandir($dir);
-		$mxid = md5(microtime(true));
-		$select =  "<select class=\"matrix-select-{$mxid}\">";
+		$select = $this->get_select_html();
+		$debug = $this->get_debug_html();
+		$help = $this->get_help_html();
+		$label = $this->get_label_html();
 
-		foreach($files as $f) {
-			if( $f != '.' && $f != '..' && file_exists($dir.'/'.$f)) {
-				$path = pathinfo($f);
+		// add it all
+		echo "
+<div class='tr-matrix control-section tr-repeater'>
+{$debug}
+<div class='matrix-controls controls'>
+{$label}
+{$select}
+<div class=\"button-group\">
+<input type=\"button\" value=\"Add New\" data-id='{$this->mxid}' data-folder='{$this->name}' class=\"button matrix-button\">
+<input type=\"button\" value=\"Flip\" class=\"flip button\">
+<input type=\"button\" value=\"Collapse\" class=\"tr_action_collapse button\">
+<input type=\"button\" value=\"Clear All\" class=\"clear button\">
+</div>
+{$help}
+</div>
+<div><input type='hidden' name='tr{$this->form->group}[{$this->name}]' /></div>
+<div class='matrix-fields matrix-fields-{$this->mxid} tr-repeater-fields ui-sortable'>";
+		$this->get_from();
+		echo "</div></div>";
 
-				if(!empty($this->include_files) && in_array($f, $this->include_files)) {
-					$select .= tr_html::element( 'option', array( 'value' => $f, 'data-file' => $path['filename']), $this->clean_file_name($path['filename']) );
-				} elseif(empty($this->include_files)) {
-					$select .= tr_html::element( 'option', array( 'value' => $f, 'data-file' => $path['filename'] ), $this->clean_file_name($path['filename']) );
-				}
+		return $this;
+	}
 
-			}
+	private function clean_file_name($name) {
+		$name = str_replace('-', ' ', $name );
+		str_replace('_', ' ', $name );
+		return ucwords($name);
+	}
+
+	private function get_help_html() {
+		if(isset($this->settings['help'])) {
+			$help =
+				"<div class=\"help\">
+          <p>{$this->settings['help']}</p>
+        </div>";
+		} else {
+			$help = '';
 		}
 
-		$select .= '</select>';
+		return $help;
+	}
 
-		// get debug
+	private function get_label_html() {
+		if(is_string($this->label)) {
+			$label = "<div class=\"control-label\"><span class=\"label\">{$this->label}</span></div>";
+		} else {
+			$label = '';
+		}
+
+		return $label;
+	}
+
+	private function get_debug_html() {
 		$debug = '';
 		if(TR_DEBUG === true && is_admin()) {
 			$debug =
@@ -68,49 +105,44 @@ class tr_matrix {
       </div>";
 		}
 
-		// add help
-		if(isset($this->settings['help'])) {
-			$help =
-				"<div class=\"help\">
-          <p>{$this->settings['help']}</p>
-        </div>";
+		return $debug;
+	}
+
+	private function get_select_html() {
+
+		$dir = __DIR__ . '/' . $this->name;
+
+		if(file_exists($dir)) {
+
+			$files = preg_grep('/^([^.])/', scandir($dir));
+
+			$select =  "<select class=\"matrix-select-{$this->mxid}\">";
+
+			foreach($files as $f) {
+				if( file_exists($dir.'/'.$f)) {
+					$path = pathinfo($f);
+
+					$in_inc_files = (!empty($this->include_files) && in_array($f, $this->include_files));
+					$no_inc_files = empty($this->include_files);
+
+					if( $in_inc_files || $no_inc_files ) {
+						$attr = array( 'value' => $f, 'data-file' => $path['filename']);
+						$select .= tr_html::element( 'option', $attr, $this->clean_file_name($path['filename']) );
+					}
+				}
+			}
+
+			$select .= '</select>';
+
 		} else {
-			$help = '';
+			$select = "<div class=\"tr-dev-alert-helper\"><i class=\"icon tr-icon-bug\"></i> Add a folder in Matrix called <code>{$this->name}</code> and add your matrix files to it.</div>";
 		}
 
-		// add label
-		$label = (is_string($this->label)) ? "<div class=\"control-label\"><span class=\"label\">{$this->label}</span></div>" : '';
+		return $select;
 
-		// add it all
-		echo "
-<div class='tr-matrix control-section tr-repeater'>
-{$debug}
-<div class='matrix-controls controls'>
-{$label}
-{$select}
-<div class=\"button-group\">
-<input type=\"button\" value=\"Add New\" data-id='$mxid' data-folder='{$this->name}' class=\"button matrix-button\">
-<input type=\"button\" value=\"Flip\" class=\"flip button\">
-<input type=\"button\" value=\"Collapse\" class=\"tr_action_collapse button\">
-<input type=\"button\" value=\"Clear All\" class=\"clear button\">
-</div>
-{$help}
-</div>
-<div><input type='hidden' name='tr{$this->form->group}[{$this->name}]' /></div>
-<div class='matrix-fields matrix-fields-$mxid tr-repeater-fields ui-sortable'>";
-		$this->get();
-		echo "</div></div>";
-
-		return $this;
 	}
 
-	private function clean_file_name($name) {
-		$name = str_replace('-', ' ', $name );
-		str_replace('_', ' ', $name );
-		return ucwords($name);
-	}
-
-	private function get() {
+	private function get_from() {
 
 		$val = (new tr_get_field())->value($this->form->group ."[{$this->name}]", $this->form->item_id, $this->form->controller, false);
 
@@ -128,10 +160,16 @@ class tr_matrix {
 					$init_grp = $form->group;
 
 					$form->group = $init_grp . "[{$tr_matrix_group}][{$tr_matrix_id}][{$tr_matrix_type}]";
+					$path = __DIR__ . "/" . $this->name . "/{$type}.php";
 
-					echo '<div class="matrix-field-group tr-repeater-group">';
-					include(__DIR__ . "/groups/{$type}.php");
-					echo '</div></div>';
+					if(file_exists($path)) {
+						echo '<div class="matrix-field-group tr-repeater-group">';
+						include($path);
+						echo '</div></div>';
+					} elseif(TR_DEBUG === true) {
+						echo "<div class=\"tr-dev-alert-helper\"><i class=\"icon tr-icon-bug\"></i> Add a file to your Matrix group <code>{$this->name}</code> and name it <code>{$type}.php</code>; ensure you require <code>tr_load_form.php</code>.</div>";
+					}
+
 
 					$form->group = $init_grp;
 					$form->debug = true;
