dbb4f1c3e9b99d2a87b3662f9279432f10c05585
kevin dees <kevin@robojuice.com>
2015-01-12 08:56:58 -0500

    config and dev help

--- plugins/matrix/tr_load_form.php
+++ plugins/matrix/tr_load_form.php
@@ -1,12 +1,22 @@
 <?php
+// test to see if this is an AJAX call
 $tr_load_ok = defined('TR_START');
 if(!$tr_load_ok) :
 
-	include( __DIR__ . '/config.php');
-	require_once(WP_INIT_PATH . '/wp-load.php');
-	require_once( get_template_directory() . '/'.TR_INIT_FOLDER.'/init.php' );
+	// test for config
+	$config = __DIR__ . '/config.php';
+	$debug = false;
+	if(file_exists($config)) {
+		require($config);
+		require_once(WP_INIT_PATH . '/wp-load.php');
+		require_once( get_template_directory() . '/'.TR_INIT_FOLDER.'/init.php' );
+	} else {
+		$debug = true;
+		echo '<div class="tr-dev-alert-helper"><i class="icon tr-icon-bug"></i>Copy the Matrix plugin <code>config.sample.php</code> file and rename it to <code>config.php</code>.</div>';
+	}
 
-	if( current_user_can('read') ) {
+	// test for user logged in
+	if( function_exists('current_user_can') && current_user_can('read') ) {
 		$tr_matrix_id = time();
 		$tr_matrix_group = $_GET['id'];
 		$tr_matrix_type = lcfirst($_GET['type']);
@@ -20,8 +30,10 @@ if(!$tr_load_ok) :
 		}
 
 		$form->group = $tr_matrix_form_group . "[{$tr_matrix_group}][{$tr_matrix_id}][{$tr_matrix_type}]";
-	} else {
+	} elseif($debug == false) {
 		http_response_code(404);
+		exit();
+	} else {
 		exit();
 	}
 
