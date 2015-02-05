10f2a21f8eb5164976bc5a534df4eb3ca1a18d19
kevin dees <kevin@robojuice.com>
2015-02-05 14:20:52 -0500

    add fix

--- core/fields/submit/class.php
+++ core/fields/submit/class.php
@@ -13,8 +13,10 @@ class tr_field_submit extends tr_field {
     unset($this->attr['value']);
     unset($this->attr['name']);
 
-    if(!isset($this->attr['class'])) {
-      $this->attr['class'] = 'button button-primary';
+    if(isset($this->attr['class'])) {
+      $this->attr['class'] .= ' button button-primary';
+    } else {
+      $this->attr['class'] = ' button button-primary';
     }
 
     return tr_html::input($this->type, $name, $value, $this->attr);
