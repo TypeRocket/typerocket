f62dc34b5f2a5c5b6e2ff94dd32dfb319b9e536a
kevindees <kevin@robojuice.com>
2015-07-26 23:02:09 -0400

    add fillable access

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -5,4 +5,16 @@ abstract class Model {
 
     private $fillable = array();
 
+    public function setFillable( $fillable, $type = 'meta' )
+    {
+        $this->fillable[$type] = $fillable;
+
+        return $this;
+    }
+
+    function getFillable($type = 'meta')
+    {
+        return $this->fillable[$type];
+    }
+
 }
