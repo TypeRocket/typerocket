b7c0b627c31581d356ffa9daaf81f2411490f325
kevin dees <kevin@robojuice.com>
2015-07-31 12:26:59 -0400

    update

--- app/api/matrix-v1.php
+++ app/api/matrix-v1.php
@@ -5,7 +5,7 @@ if($tr_loaded) {
     $matrix_group = get_query_var('typerocket_matrix_group', null);
     $matrix_type = get_query_var('typerocket_matrix_type', null);
 
-    $load = apply_filters('tr_matrix_api_load', true, $matrix_group, $matrix_type);
+    $load = apply_filters('tr_matrix_api_load', true, $matrix_group, $matrix_type, $_POST['form_group']);
     if($load) {
         new TypeRocket\Http\Matrix($matrix_group, $matrix_type, $_POST['form_group']);
     }
