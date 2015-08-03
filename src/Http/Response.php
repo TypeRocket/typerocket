0cdc3728ed84bcb03bb3ed16bd446f66c2fe9da9
kevindees <kevin@robojuice.com>
2015-08-02 20:35:10 -0400

    comment

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,6 +1,17 @@
 <?php
 namespace TypeRocket\Http;
 
+/**
+ * Class Response
+ *
+ * The Response class is not designed for PSR-7
+ *
+ * This class is designed to give hooks for the json
+ * response sent back by the TypeRocket AJAX REST
+ * API.
+ *
+ * @package TypeRocket\Http
+ */
 class Response {
 
     private $message = 'No Response';
@@ -11,12 +22,25 @@ class Response {
     private $errors = array();
     private $data = array();
 
+    /**
+     * Set HTTP status code
+     *
+     * @param $status
+     */
     public function setStatus( $status )
     {
         $this->status = (int) $status;
     }
 
-
+    /**
+     * Set message property
+     *
+     * This is the message seen in the flash alert.
+     *
+     * @param $message
+     *
+     * @return $this
+     */
     public function setMessage( $message )
     {
         $this->message = (string) $message;
@@ -24,6 +48,16 @@ class Response {
         return $this;
     }
 
+    /**
+     * Set redirect
+     *
+     * Redirect the user to a new url. This only works when using AJAX
+     * REST API on a Form.
+     *
+     * @param $url
+     *
+     * @return $this
+     */
     public function setRedirect( $url )
     {
         $this->redirect = $url;
@@ -31,6 +65,15 @@ class Response {
         return $this;
     }
 
+    /**
+     * Set Invalid
+     *
+     * Set the response as invalid. This does not return a 404 status
+     * or a 401 status. If is only a hook to check if a request
+     * is valid.
+     *
+     * @return $this
+     */
     public function setInvalid()
     {
         $this->valid = false;
@@ -38,6 +81,15 @@ class Response {
         return $this;
     }
 
+    /**
+     * Set Flash
+     *
+     * Set if the flash message should be shown on the front end.
+     *
+     * @param bool|true $flash
+     *
+     * @return $this
+     */
     public function setFlash( $flash = true )
     {
         $this->flash = (bool) $flash;
@@ -45,6 +97,15 @@ class Response {
         return $this;
     }
 
+    /**
+     * Set Errors
+     *
+     * Set errors to help front-end developers
+     *
+     * @param array $errors
+     *
+     * @return $this
+     */
     public function setErrors( array $errors )
     {
         $this->errors = $errors;
@@ -52,12 +113,54 @@ class Response {
         return $this;
     }
 
+    /**
+     * Get Errors
+     *
+     * @return array
+     */
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    /**
+     * Get Error by key
+     *
+     * @param $key
+     *
+     * @return array
+     */
+    public function getError($key) {
+
+        $error = null;
+
+        if(array_key_exists($key, $this->errors)) {
+            $error = $this->errors[$key];
+        }
+
+        return $error;
+    }
+
+    /**
+     * Set Error by key
+     *
+     * @param $key
+     * @param $value
+     *
+     * @return $this
+     */
     public function setError($key, $value) {
         $this->errors[$key] = $value;
 
         return $this;
     }
 
+    /**
+     * Remove Error by key
+     *
+     * @param $key
+     *
+     * @return $this
+     */
     public function removeError($key) {
 
         if(array_key_exists($key, $this->errors)) {
@@ -68,6 +171,16 @@ class Response {
 
     }
 
+    /**
+     * Set Data by key
+     *
+     * Set the data to return for front-end developers. This should
+     * be data used to describe what was updated or created for
+     * example.
+     * 
+     * @param $key
+     * @param $data
+     */
     public function setData( $key, $data ) {
         $this->data[$key] = $data;
     }
@@ -78,10 +191,6 @@ class Response {
 
     public function getMessage() {
         return $this->message;
-    }
-
-    public function getErrors() {
-        return $this->errors;
     }
 
     public function getData() {
