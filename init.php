cda9825be2677449acee8d690b26551f95383cbd
kevin dees <kevin@robojuice.com>
2016-04-07 14:38:24 -0400

    update versions restriction for wp and php

--- init.php
+++ init.php
@@ -4,7 +4,7 @@
 | TypeRocket by Robojuice
 |--------------------------------------------------------------------------
 |
-| TypeRocket is designed to work with WordPress 4.0+ and PHP 5.3+. You
+| TypeRocket is designed to work with WordPress 4.5+ and PHP 5.4+. You
 | must initialize it exactly once. We suggest including TypeRocket
 | from your theme and let plugins access TypeRocket from there.
 |
@@ -17,15 +17,15 @@
 | Require TypeRocket
 |--------------------------------------------------------------------------
 |
-| Try to start the TypeRocket. Need WordPress 4.0+ and PHP 5.3+
+| Try to start the TypeRocket. Need WordPress 4.5+ and PHP 5.4+
 |
 */
 global $wp_version;
 
-if(empty($wp_version) || $wp_version < '4'  ) :
-  die("You need version 4.0+ of WordPress. Using " . $wp_version);
-elseif(PHP_VERSION < '5.3') :
-  die("You need version 5.3+ of PHP. Using " . PHP_VERSION);
+if(empty($wp_version) || $wp_version < '4.5'  ) :
+  die("You need version 4.5+ of WordPress. Using " . $wp_version);
+elseif(PHP_VERSION < '5.4') :
+  die("You need version 5.4+ of PHP. Using " . PHP_VERSION);
 elseif(!defined('TR_START')) :
   require __DIR__ . '/core/start.php';
 endif;
