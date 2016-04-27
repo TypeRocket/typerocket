9fa9b3043882fffd49d476dc9845bed5dd148a03
kevin dees <kevin@robojuice.com>
2016-04-27 11:23:49 -0400

    add Tags model and controller

---
+++ src/Controllers/TagsController.php
@@ -0,0 +1,6 @@
+<?php
+namespace TypeRocket\Controllers;
+
+class TagsController extends TaxonomiesController
+{
+}
---
+++ src/Models/TagsModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TypeRocket\Models;
+
+class TagsModel extends TaxonomiesModel
+{
+    protected $taxonomy = 'post_tag';
+}
