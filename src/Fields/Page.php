d26d3db9fe299964aae91d2583229919614db667
kevin dees <kevin@robojuice.com>
2016-04-27 12:28:30 -0400

    add defautl option for setting

---
+++ src/Fields/Page.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace TypeRocket\Fields;
+
+use TypeRocket\Html\Generator;
+
+class Page extends Field
+{
+
+    /**
+     * Run on construction
+     */
+    protected function init()
+    {
+        $this->setType( 'text' );
+    }
+
+    /**
+     * Covert Test to HTML string
+     */
+    public function getString()
+    {
+        $max = '';
+        $input = new Generator();
+        $name = $this->getNameAttributeString();
+        $value = $this->getValue();
+        $sanitize = "\\TypeRocket\\Sanitize::" . $this->getSetting('sanitize', 'raw');
+
+        if ( is_callable($sanitize)) {
+            $value = esc_attr( call_user_func($sanitize, $value ) );
+        }
+
+        $maxLength = $this->getAttribute('maxlength');
+
+        if ($maxLength != null && $maxLength > 0) {
+            $left = (int) $maxLength - strlen( utf8_decode( $value ) );
+            $max = new Generator();
+            $max->newElement('p', array('class' => 'tr-maxlength'), 'Characters left: ')->appendInside('span', array(), $left);
+            $max = $max->getString();
+        }
+
+        return $input->newInput($this->getType(), $name, $value, $this->getAttributes() )->getString() . $max;
+    }
+
+}
--- src/Traits/FormConnectorTrait.php
+++ src/Traits/FormConnectorTrait.php
@@ -173,13 +173,14 @@ trait FormConnectorTrait
      * Get From setting by key
      *
      * @param $key
+     * @param null $default default value to return if none
      *
      * @return null
      */
-    public function getSetting( $key )
+    public function getSetting( $key, $default = null )
     {
         if ( ! array_key_exists( $key, $this->settings )) {
-            return null;
+            return $default;
         }
 
         return $this->settings[$key];
