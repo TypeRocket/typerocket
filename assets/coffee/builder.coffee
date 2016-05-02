e128bc34fcf9d5fcbe3a3c3a22a0c42e881fe9ab
kevin dees <kevin@robojuice.com>
2016-05-02 09:13:19 -0400

    fix builder bugs and update design

--- assets/coffee/builder.coffee
+++ assets/coffee/builder.coffee
@@ -1,117 +1,119 @@
 jQuery(document).ready ($) ->
 
-  initComponent = (data, fields) ->
-    callbacks = TypeRocket.repeaterCallbacks
-    ri = 0
-    while callbacks.length > ri
-      if typeof callbacks[ri] == 'function'
-        callbacks[ri] data
-      ri++
-    if $.isFunction($.fn.sortable)
-      $sortables = fields.find('.tr-gallery-list')
-      $items_list = fields.find('.tr-items-list')
-      $repeater_fields = fields.find('.tr-repeater-fields')
-      if $sortables.length > 0
-        $sortables.sortable()
-      if $repeater_fields.length > 0
-        $repeater_fields.sortable
-          connectWith: '.tr-repeater-group'
-          handle: '.repeater-controls'
-      if $items_list.length > 0
-        $items_list.sortable
-          connectWith: '.item'
-          handle: '.move'
-    return
+  if $('.tr-components').length > 0
+
+    initComponent = (data, fields) ->
+      callbacks = TypeRocket.repeaterCallbacks
+      ri = 0
+      while callbacks.length > ri
+        if typeof callbacks[ri] == 'function'
+          callbacks[ri] data
+        ri++
+      if $.isFunction($.fn.sortable)
+        $sortables = fields.find('.tr-gallery-list')
+        $items_list = fields.find('.tr-items-list')
+        $repeater_fields = fields.find('.tr-repeater-fields')
+        if $sortables.length > 0
+          $sortables.sortable()
+        if $repeater_fields.length > 0
+          $repeater_fields.sortable
+            connectWith: '.tr-repeater-group'
+            handle: '.repeater-controls'
+        if $items_list.length > 0
+          $items_list.sortable
+            connectWith: '.item'
+            handle: '.move'
+      return
 
 
-  $('.typerocket-container').on 'click', '.tr-builder-add-button', (e) ->
-    e.preventDefault()
-    select = $(this).next()
+    $('.typerocket-container').on 'click', '.tr-builder-add-button', (e) ->
+      e.preventDefault()
+      select = $(this).next()
 
-    overlay = $('<div>').addClass('tr-builder-select-overlay').on 'click', ->
-      $(this).remove()
-      $('.tr-builder-select').fadeOut()
+      overlay = $('<div>').addClass('tr-builder-select-overlay').on 'click', ->
+        $(this).remove()
+        $('.tr-builder-select').fadeOut()
 
-    $('body').append overlay
-    select.fadeIn()
+      $('body').append overlay
+      select.fadeIn()
 
-  $('.typerocket-container').on 'click', '.tr-builder-component-control', (e) ->
-    e.preventDefault()
-    $(this).parent().children().removeClass 'active'
-    id = $(this).addClass('active').parent().data 'id'
-    index = $(this).index()
-    frame = $('#frame-'+id)
-    components = frame.children()
-    components.removeClass 'active'
-    component = components[index]
-    $(component).addClass 'active'
-
-  $('.typerocket-container').on 'click', '.tr-remove-builder-component', (e) ->
-    e.preventDefault()
-    if confirm('Remove component?')
-      control = $(this).parent()
-      control.parent().children().removeClass 'active'
-      id = control.parent().data 'id'
-      index = $(this).parent().index()
+    $('.typerocket-container').on 'click', '.tr-builder-component-control', (e) ->
+      e.preventDefault()
+      $(this).parent().children().removeClass 'active'
+      id = $(this).addClass('active').parent().data 'id'
+      index = $(this).index()
       frame = $('#frame-'+id)
       components = frame.children()
+      components.removeClass 'active'
       component = components[index]
-      $(component).remove()
-      control.remove()
+      $(component).addClass 'active'
 
-  $('.tr-components').sortable
-    start: (e, ui) ->
-      ui.item.startPos = ui.item.index()
+    $('.typerocket-container').on 'click', '.tr-remove-builder-component', (e) ->
+      e.preventDefault()
+      if confirm('Remove component?')
+        control = $(this).parent()
+        control.parent().children().removeClass 'active'
+        id = control.parent().data 'id'
+        index = $(this).parent().index()
+        frame = $('#frame-'+id)
+        components = frame.children()
+        component = components[index]
+        $(component).remove()
+        control.remove()
 
-    update: (e, ui) ->
-      select = ui.item.parent()
-      id = select.data 'id'
-      frame = $('#frame-'+id)
-      components = frame.children().detach()
-      index = ui.item.index()
-      old = ui.item.startPos
-      builder = components.splice old, 1
-      components.splice index, 0, builder[0]
-      frame.append components
+    $('.tr-components').sortable
+      start: (e, ui) ->
+        ui.item.startPos = ui.item.index()
+
+      update: (e, ui) ->
+        select = ui.item.parent()
+        id = select.data 'id'
+        frame = $('#frame-'+id)
+        components = frame.children().detach()
+        index = ui.item.index()
+        old = ui.item.startPos
+        builder = components.splice old, 1
+        components.splice index, 0, builder[0]
+        frame.append components
 
 
-  $('.typerocket-container').on 'click', '.builder-select-option', (e) ->
-    $that = $(this)
-    $that.parent().fadeOut()
-    $('.tr-builder-select-overlay').remove()
-    if !$that.hasClass('disabled')
-      mxid = $that.data('id')
-      group = $that.data('folder')
-      img = $that.data('thumbnail')
-      $fields = $('#frame-' + mxid)
-      $components = $('#components-' + mxid)
-      $select = $('ul[data-mxid="' + mxid + '"]')
-      type = $that.data('value')
-      $that.addClass 'disabled'
-      url = '/typerocket_builder_api/v1/' + group + '/' + type
-      form_group = $select.data('group')
-      $.ajax
-        url: url
-        method: 'POST'
-        dataType: 'html'
-        data: form_group: form_group
-        success: (data) ->
-          data = $(data)
-          $fields.children().removeClass 'active'
-          $components.children().removeClass 'active'
-          data.prependTo($fields).addClass 'active'
+    $('.typerocket-container').on 'click', '.builder-select-option', (e) ->
+      $that = $(this)
+      $that.parent().fadeOut()
+      $('.tr-builder-select-overlay').remove()
+      if !$that.hasClass('disabled')
+        mxid = $that.data('id')
+        group = $that.data('folder')
+        img = $that.data('thumbnail')
+        $fields = $('#frame-' + mxid)
+        $components = $('#components-' + mxid)
+        $select = $('ul[data-mxid="' + mxid + '"]')
+        type = $that.data('value')
+        $that.addClass 'disabled'
+        url = '/typerocket_builder_api/v1/' + group + '/' + type
+        form_group = $select.data('group')
+        $.ajax
+          url: url
+          method: 'POST'
+          dataType: 'html'
+          data: form_group: form_group
+          success: (data) ->
+            data = $(data)
+            $fields.children().removeClass 'active'
+            $components.children().removeClass 'active'
+            data.prependTo($fields).addClass 'active'
 
-          if img
-            img = '<img src="'+img+'" />'
+            if img
+              img = '<img src="'+img+'" />'
 
-          html = '
-<li class="active tr-builder-component-control">'+img+'<span class="tr-builder-component-title">'+$that.text()+'</span><span class="remove tr-remove-builder-component"></span>
-'
-          $components.prepend html
-          initComponent data, $fields
-          $that.removeClass 'disabled'
-        error: (jqXHR) ->
-          $that.val('Try again - Error ' + jqXHR.status).removeAttr 'disabled', 'disabled'
-          return
+            html = '
+  <li class="active tr-builder-component-control">'+img+'<span class="tr-builder-component-title">'+$that.text()+'</span><span class="remove tr-remove-builder-component"></span>
+  '
+            $components.prepend html
+            initComponent data, $fields
+            $that.removeClass 'disabled'
+          error: (jqXHR) ->
+            $that.val('Try again - Error ' + jqXHR.status).removeAttr 'disabled', 'disabled'
+            return
+      return
     return
-  return
--- assets/js/typerocket.js
+++ assets/js/typerocket.js
@@ -608,135 +608,137 @@
 (function() {
   jQuery(document).ready(function($) {
     var initComponent;
-    initComponent = function(data, fields) {
-      var $items_list, $repeater_fields, $sortables, callbacks, ri;
-      callbacks = TypeRocket.repeaterCallbacks;
-      ri = 0;
-      while (callbacks.length > ri) {
-        if (typeof callbacks[ri] === 'function') {
-          callbacks[ri](data);
+    if ($('.tr-components').length > 0) {
+      initComponent = function(data, fields) {
+        var $items_list, $repeater_fields, $sortables, callbacks, ri;
+        callbacks = TypeRocket.repeaterCallbacks;
+        ri = 0;
+        while (callbacks.length > ri) {
+          if (typeof callbacks[ri] === 'function') {
+            callbacks[ri](data);
+          }
+          ri++;
         }
-        ri++;
-      }
-      if ($.isFunction($.fn.sortable)) {
-        $sortables = fields.find('.tr-gallery-list');
-        $items_list = fields.find('.tr-items-list');
-        $repeater_fields = fields.find('.tr-repeater-fields');
-        if ($sortables.length > 0) {
-          $sortables.sortable();
+        if ($.isFunction($.fn.sortable)) {
+          $sortables = fields.find('.tr-gallery-list');
+          $items_list = fields.find('.tr-items-list');
+          $repeater_fields = fields.find('.tr-repeater-fields');
+          if ($sortables.length > 0) {
+            $sortables.sortable();
+          }
+          if ($repeater_fields.length > 0) {
+            $repeater_fields.sortable({
+              connectWith: '.tr-repeater-group',
+              handle: '.repeater-controls'
+            });
+          }
+          if ($items_list.length > 0) {
+            $items_list.sortable({
+              connectWith: '.item',
+              handle: '.move'
+            });
+          }
         }
-        if ($repeater_fields.length > 0) {
-          $repeater_fields.sortable({
-            connectWith: '.tr-repeater-group',
-            handle: '.repeater-controls'
-          });
-        }
-        if ($items_list.length > 0) {
-          $items_list.sortable({
-            connectWith: '.item',
-            handle: '.move'
-          });
-        }
-      }
-    };
-    $('.typerocket-container').on('click', '.tr-builder-add-button', function(e) {
-      var overlay, select;
-      e.preventDefault();
-      select = $(this).next();
-      overlay = $('<div>').addClass('tr-builder-select-overlay').on('click', function() {
-        $(this).remove();
-        return $('.tr-builder-select').fadeOut();
+      };
+      $('.typerocket-container').on('click', '.tr-builder-add-button', function(e) {
+        var overlay, select;
+        e.preventDefault();
+        select = $(this).next();
+        overlay = $('<div>').addClass('tr-builder-select-overlay').on('click', function() {
+          $(this).remove();
+          return $('.tr-builder-select').fadeOut();
+        });
+        $('body').append(overlay);
+        return select.fadeIn();
       });
-      $('body').append(overlay);
-      return select.fadeIn();
-    });
-    $('.typerocket-container').on('click', '.tr-builder-component-control', function(e) {
-      var component, components, frame, id, index;
-      e.preventDefault();
-      $(this).parent().children().removeClass('active');
-      id = $(this).addClass('active').parent().data('id');
-      index = $(this).index();
-      frame = $('#frame-' + id);
-      components = frame.children();
-      components.removeClass('active');
-      component = components[index];
-      return $(component).addClass('active');
-    });
-    $('.typerocket-container').on('click', '.tr-remove-builder-component', function(e) {
-      var component, components, control, frame, id, index;
-      e.preventDefault();
-      if (confirm('Remove component?')) {
-        control = $(this).parent();
-        control.parent().children().removeClass('active');
-        id = control.parent().data('id');
-        index = $(this).parent().index();
+      $('.typerocket-container').on('click', '.tr-builder-component-control', function(e) {
+        var component, components, frame, id, index;
+        e.preventDefault();
+        $(this).parent().children().removeClass('active');
+        id = $(this).addClass('active').parent().data('id');
+        index = $(this).index();
         frame = $('#frame-' + id);
         components = frame.children();
+        components.removeClass('active');
         component = components[index];
-        $(component).remove();
-        return control.remove();
-      }
-    });
-    $('.tr-components').sortable({
-      start: function(e, ui) {
-        return ui.item.startPos = ui.item.index();
-      },
-      update: function(e, ui) {
-        var builder, components, frame, id, index, old, select;
-        select = ui.item.parent();
-        id = select.data('id');
-        frame = $('#frame-' + id);
-        components = frame.children().detach();
-        index = ui.item.index();
-        old = ui.item.startPos;
-        builder = components.splice(old, 1);
-        components.splice(index, 0, builder[0]);
-        return frame.append(components);
-      }
-    });
-    $('.typerocket-container').on('click', '.builder-select-option', function(e) {
-      var $components, $fields, $select, $that, form_group, group, img, mxid, type, url;
-      $that = $(this);
-      $that.parent().fadeOut();
-      $('.tr-builder-select-overlay').remove();
-      if (!$that.hasClass('disabled')) {
-        mxid = $that.data('id');
-        group = $that.data('folder');
-        img = $that.data('thumbnail');
-        $fields = $('#frame-' + mxid);
-        $components = $('#components-' + mxid);
-        $select = $('ul[data-mxid="' + mxid + '"]');
-        type = $that.data('value');
-        $that.addClass('disabled');
-        url = '/typerocket_builder_api/v1/' + group + '/' + type;
-        form_group = $select.data('group');
-        $.ajax({
-          url: url,
-          method: 'POST',
-          dataType: 'html',
-          data: {
-            form_group: form_group
-          },
-          success: function(data) {
-            var html;
-            data = $(data);
-            $fields.children().removeClass('active');
-            $components.children().removeClass('active');
-            data.prependTo($fields).addClass('active');
-            if (img) {
-              img = '<img src="' + img + '" />';
+        return $(component).addClass('active');
+      });
+      $('.typerocket-container').on('click', '.tr-remove-builder-component', function(e) {
+        var component, components, control, frame, id, index;
+        e.preventDefault();
+        if (confirm('Remove component?')) {
+          control = $(this).parent();
+          control.parent().children().removeClass('active');
+          id = control.parent().data('id');
+          index = $(this).parent().index();
+          frame = $('#frame-' + id);
+          components = frame.children();
+          component = components[index];
+          $(component).remove();
+          return control.remove();
+        }
+      });
+      $('.tr-components').sortable({
+        start: function(e, ui) {
+          return ui.item.startPos = ui.item.index();
+        },
+        update: function(e, ui) {
+          var builder, components, frame, id, index, old, select;
+          select = ui.item.parent();
+          id = select.data('id');
+          frame = $('#frame-' + id);
+          components = frame.children().detach();
+          index = ui.item.index();
+          old = ui.item.startPos;
+          builder = components.splice(old, 1);
+          components.splice(index, 0, builder[0]);
+          return frame.append(components);
+        }
+      });
+      $('.typerocket-container').on('click', '.builder-select-option', function(e) {
+        var $components, $fields, $select, $that, form_group, group, img, mxid, type, url;
+        $that = $(this);
+        $that.parent().fadeOut();
+        $('.tr-builder-select-overlay').remove();
+        if (!$that.hasClass('disabled')) {
+          mxid = $that.data('id');
+          group = $that.data('folder');
+          img = $that.data('thumbnail');
+          $fields = $('#frame-' + mxid);
+          $components = $('#components-' + mxid);
+          $select = $('ul[data-mxid="' + mxid + '"]');
+          type = $that.data('value');
+          $that.addClass('disabled');
+          url = '/typerocket_builder_api/v1/' + group + '/' + type;
+          form_group = $select.data('group');
+          $.ajax({
+            url: url,
+            method: 'POST',
+            dataType: 'html',
+            data: {
+              form_group: form_group
+            },
+            success: function(data) {
+              var html;
+              data = $(data);
+              $fields.children().removeClass('active');
+              $components.children().removeClass('active');
+              data.prependTo($fields).addClass('active');
+              if (img) {
+                img = '<img src="' + img + '" />';
+              }
+              html = '<li class="active tr-builder-component-control">' + img + '<span class="tr-builder-component-title">' + $that.text() + '</span><span class="remove tr-remove-builder-component"></span>';
+              $components.prepend(html);
+              initComponent(data, $fields);
+              return $that.removeClass('disabled');
+            },
+            error: function(jqXHR) {
+              $that.val('Try again - Error ' + jqXHR.status).removeAttr('disabled', 'disabled');
             }
-            html = '<li class="active tr-builder-component-control">' + img + '<span class="tr-builder-component-title">' + $that.text() + '</span><span class="remove tr-remove-builder-component"></span>';
-            $components.prepend(html);
-            initComponent(data, $fields);
-            return $that.removeClass('disabled');
-          },
-          error: function(jqXHR) {
-            $that.val('Try again - Error ' + jqXHR.status).removeAttr('disabled', 'disabled');
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+    }
   });
 
 }).call(this);
