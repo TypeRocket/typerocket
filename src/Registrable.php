4fa369709d169b50a0d0193fa08825a1d5fd5186
kevin dees <kevin@robojuice.com>
2016-07-09 12:44:06 -0400

    refector

--- src/Registrable.php
+++ src/Registrable.php
@@ -4,96 +4,96 @@ namespace TypeRocket;
 abstract class Registrable
 {
 
-    protected $use = array();
+    protected $use = [];
     protected $id = null;
-    protected $args = array();
-    protected $reservedNames = array(
-        'attachment'                  => true,
-        'attachment_id'               => true,
-        'author'                      => true,
-        'author_name'                 => true,
-        'action'                      => true,
-        'calendar'                    => true,
-        'cat'                         => true,
-        'category'                    => true,
-        'category__and'               => true,
-        'category__in'                => true,
-        'category__not_in'            => true,
-        'category_name'               => true,
-        'comments_per_page'           => true,
-        'comments_popup'              => true,
-        'customize_messenger_channel' => true,
-        'customized'                  => true,
-        'cpage'                       => true,
-        'day'                         => true,
-        'debug'                       => true,
-        'error'                       => true,
-        'exact'                       => true,
-        'feed'                        => true,
-        'hour'                        => true,
-        'link_category'               => true,
-        'm'                           => true,
-        'minute'                      => true,
-        'monthnum'                    => true,
-        'more'                        => true,
-        'name'                        => true,
-        'nav_menu'                    => true,
-        'nonce'                       => true,
-        'nopaging'                    => true,
-        'offset'                      => true,
-        'order'                       => true,
-        'orderby'                     => true,
-        'p'                           => true,
-        'page'                        => true,
-        'page_id'                     => true,
-        'paged'                       => true,
-        'pagename'                    => true,
-        'pb'                          => true,
-        'perm'                        => true,
-        'post'                        => true,
-        'post__in'                    => true,
-        'post__not_in'                => true,
-        'post_format'                 => true,
-        'post_mime_type'              => true,
-        'post_status'                 => true,
-        'post_tag'                    => true,
-        'post_type'                   => true,
-        'posts'                       => true,
-        'posts_per_archive_page'      => true,
-        'posts_per_page'              => true,
-        'preview'                     => true,
-        'robots'                      => true,
-        's'                           => true,
-        'search'                      => true,
-        'second'                      => true,
-        'sentence'                    => true,
-        'showposts'                   => true,
-        'static'                      => true,
-        'subpost'                     => true,
-        'subpost_id'                  => true,
-        'tag'                         => true,
-        'tag__and'                    => true,
-        'tag__in'                     => true,
-        'tag__not_in'                 => true,
-        'tag_id'                      => true,
-        'tag_slug__and'               => true,
-        'tag_slug__in'                => true,
-        'taxonomy'                    => true,
-        'tb'                          => true,
-        'term'                        => true,
-        'theme'                       => true,
-        'type'                        => true,
-        'w'                           => true,
-        'withcomments'                => true,
-        'withoutcomments'             => true,
-        'year'                        => true
-    );
+    protected $args = [];
+    protected $reservedNames = [
+        'attachment',
+        'attachment_id',
+        'author',
+        'author_name',
+        'action',
+        'calendar',
+        'cat',
+        'category',
+        'category__and',
+        'category__in',
+        'category__not_in',
+        'category_name',
+        'comments_per_page',
+        'comments_popup',
+        'customize_messenger_channel',
+        'customized',
+        'cpage',
+        'day',
+        'debug',
+        'error',
+        'exact',
+        'feed',
+        'hour',
+        'link_category',
+        'm',
+        'minute',
+        'monthnum',
+        'more',
+        'name',
+        'nav_menu',
+        'nonce',
+        'nopaging',
+        'offset',
+        'order',
+        'orderby',
+        'p',
+        'page',
+        'page_id',
+        'paged',
+        'pagename',
+        'pb',
+        'perm',
+        'post',
+        'post__in',
+        'post__not_in',
+        'post_format',
+        'post_mime_type',
+        'post_status',
+        'post_tag',
+        'post_type',
+        'posts',
+        'posts_per_archive_page',
+        'posts_per_page',
+        'preview',
+        'robots',
+        's',
+        'search',
+        'second',
+        'sentence',
+        'showposts',
+        'static',
+        'subpost',
+        'subpost_id',
+        'tag',
+        'tag__and',
+        'tag__in',
+        'tag__not_in',
+        'tag_id',
+        'tag_slug__and',
+        'tag_slug__in',
+        'taxonomy',
+        'tb',
+        'term',
+        'theme',
+        'type',
+        'w',
+        'withcomments',
+        'withoutcomments',
+        'year'
+    ];
 
-    public function __get( $property )
+    public function __get($property)
     {
     }
 
-    public function __set( $property, $value )
+    public function __set($property, $value)
     {
     }
 
@@ -104,14 +104,10 @@ abstract class Registrable
      *
      * @return $this
      */
-    public function setId( $id )
+    public function setId($id)
     {
-
-        if(in_array($id, $this->reservedNames)) {
-            $this->id = Sanitize::underscore( $id );
-        } else {
-            die('ID Reserved: ' . $id);
-        }
+        $this->id = Sanitize::underscore($id);
+        $this->dieIfReserved();
 
         return $this;
     }
@@ -133,7 +129,7 @@ abstract class Registrable
      *
      * @return $this
      */
-    public function setArguments( array $args )
+    public function setArguments(array $args)
     {
         $this->args = $args;
 
@@ -155,9 +151,9 @@ abstract class Registrable
      *
      * @return string
      */
-    public function getArgument( $key )
+    public function getArgument($key)
     {
-        if( ! array_key_exists($key, $this->args)) {
+        if ( ! array_key_exists($key, $this->args)) {
             return null;
         }
 
@@ -172,7 +168,7 @@ abstract class Registrable
      *
      * @return $this
      */
-    public function setArgument( $key, $value )
+    public function setArgument($key, $value)
     {
 
         $this->args[$key] = $value;
@@ -187,9 +183,9 @@ abstract class Registrable
      *
      * @return $this
      */
-    public function removeArgument( $key )
+    public function removeArgument($key)
     {
-        if( array_key_exists($key, $this->args)) {
+        if (array_key_exists($key, $this->args)) {
             unset($this->args[$key]);
         }
 
@@ -198,8 +194,8 @@ abstract class Registrable
 
     protected function dieIfReserved()
     {
-        if (array_key_exists( $this->id, $this->reservedNames )) {
-            die( 'TypeRocket: Error, you are using the reserved wp name "' . $this->id . '".' );
+        if (in_array($this->id, $this->reservedNames)) {
+            die('TypeRocket: Error, you are using the reserved wp name "' . $this->id . '".');
         }
     }
 
@@ -210,15 +206,15 @@ abstract class Registrable
      *
      * @return $this
      */
-    public function apply( $args )
+    public function apply($args)
     {
 
-        if( ! is_array($args)) {
+        if ( ! is_array($args)) {
             $args = func_get_args();
         }
 
-        if ( ! empty($args) && is_array( $args ) ) {
-            $this->use = array_merge( $this->use, $args );
+        if ( ! empty($args) && is_array($args)) {
+            $this->use = array_merge($this->use, $args);
         }
 
         $this->uses();
@@ -233,7 +229,7 @@ abstract class Registrable
      */
     public function addToRegistry()
     {
-        Registry::addRegistrable( $this );
+        Registry::addRegistrable($this);
 
         return $this;
     }
@@ -255,14 +251,14 @@ abstract class Registrable
 
         foreach ($this->use as $obj) {
             if ($obj instanceof Registrable) {
-                $class  = get_class( $obj );
-                $class = substr($class,11);
+                $class  = get_class($obj);
+                $class  = substr($class, 11);
                 $method = 'add' . $class;
-                if (method_exists( $this, $method )) {
-                    $this->$method( $obj );
+                if (method_exists($this, $method)) {
+                    $this->$method($obj);
                 } else {
-                    $current_class = get_class( $this );
-                    die( 'TypeRocket: You are passing the unsupported object ' . $class . ' into ' . $current_class . '.' );
+                    $current_class = get_class($this);
+                    die('TypeRocket: You are passing the unsupported object ' . $class . ' into ' . $current_class . '.');
                 }
             }
         }
