2a9e1120f4eae97fb976583e98fd966703fa0f6f
kevin dees <kevin@robojuice.com>
2016-08-02 20:34:32 -0400

    add comments to view class

--- src/View.php
+++ src/View.php
@@ -8,6 +8,14 @@ class View
     static public $file = null;
     static public $template = null;
 
+    /**
+     * View constructor.
+     *
+     * Take a custom file location or dot notation of view location.
+     *
+     * @param $file
+     * @param array $data
+     */
     public function __construct( $file , array $data = [] )
     {
         if( file_exists($file) ) {
@@ -24,14 +32,33 @@ class View
         }
     }
 
+    /**
+     * Get the file
+     *
+     * This is used for admin pages
+     *
+     * @return null|string
+     */
     public function file() {
         return self::$file;
     }
 
+    /**
+     * Get the template
+     *
+     * This is used for front-end views
+     *
+     * @return null|string
+     */
     public function template() {
         return self::$template;
     }
 
+    /**
+     * Get the data attached to a view.
+     *
+     * @return array
+     */
     public function data()
     {
         return self::$data;
