dfd255766eca6b7f5bc3ee23e034b421e6544a32
kevin dees <kevin@robojuice.com>
2016-08-06 20:09:34 -0400

    add missing config

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| TypeRocket URL
+|--------------------------------------------------------------------------
+|
+| The URL where TypeRocket can be found.
+|
+*/
+define('TR_URL', get_stylesheet_directory_uri() . '/typerocket');
+
+/*
+|--------------------------------------------------------------------------
+| Debug
+|--------------------------------------------------------------------------
+|
+| Turn on Debugging for TypeRocket. Set to false to disable.
+|
+*/
+define('TR_DEBUG', true);
+
+/*
+|--------------------------------------------------------------------------
+| Enabled Plugins
+|--------------------------------------------------------------------------
+|
+| The folder names of the TypeRocket plugins you wish to enable separated
+| by pipes: seo|dev|theme-options|builder
+|
+*/
+define('TR_PLUGINS', 'theme-options|seo');
+
+/*
+|--------------------------------------------------------------------------
+| Seed
+|--------------------------------------------------------------------------
+|
+| A 'random' string of text to help with security from time to time.
+|
+*/
+define('TR_SEED', 'PUT_TYPEROCKET_SEED_HERE');
