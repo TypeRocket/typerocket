7df323576a8c3a4c834997a8c3be5bea146739c9
kevin dees <kevin@robojuice.com>
2016-08-08 10:57:53 -0400

    add cli and tiny tweaks

--- app/Models/Pages.php
+++ app/Models/Pages.php
@@ -5,6 +5,5 @@ use TypeRocket\Models\PostTypesModel;
 
 class Pages extends PostTypesModel
 {
-    /** @var string $postType set as correct post type for model */
     protected $postType = 'page';
 }
