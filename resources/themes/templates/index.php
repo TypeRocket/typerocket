1b4d2aaef6d61984974f34a9462ab9751b2b610c
kevin dees <kevin@robojuice.com>
2016-08-10 09:23:20 -0400

    update templates index design

--- resources/themes/templates/index.php
+++ resources/themes/templates/index.php
@@ -5,12 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TypeRocket 3.0</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/typerocket-public/galaxy/galaxy.css">
+    <title>TypeRocket Galaxy - The WordPress Framework Designed for Developers</title>
     <?php wp_head(); ?>
 </head>
 <body>
-<h1>TypeRocket</h1>
-<p>The WordPress Framework Designed for Developers.</p>
+
+<section>
+    <img src="https://s3-us-west-2.amazonaws.com/typerocket-public/galaxy/typerocket-logo.svg" alt="TypeRocket Galaxy">
+    <p>Galaxy</p>
+</section>
+
 <?php wp_footer(); ?>
 </body>
 </html>
