f64ce493610aeec4c0638e14c76d9c631d94f03b
kevin dees <kevin@robojuice.com>
2016-08-11 16:51:13 -0400

    update galaxy config

--- config/galaxy.php
+++ config/galaxy.php
@@ -7,8 +7,10 @@ return [
     |
     | Set to the WordPress root directory load file wp-load.php.
     |
+    | Example: TR_PATH . '/wordpress/wp-load.php'
+    |
     */
-    'wordpress' => TR_PATH . '/wordpress/wp-load.php',
+    'wordpress' => false,
 
     /*
     |--------------------------------------------------------------------------
