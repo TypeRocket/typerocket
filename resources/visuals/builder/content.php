c0cefd7bb25ef8b821e7fc9d9b02656deec9e613
kevin dees <kevin@robojuice.com>
2016-09-15 15:07:21 -0400

    fix design bugs

--- resources/visuals/builder/content.php
+++ resources/visuals/builder/content.php
@@ -1,5 +1,5 @@
 <div class="builder-content">
     <h2><?php echo esc_html($data['headline']); ?></h2>
     <hr />
-    <?php echo wpautop( esc_html($data['content']) ); ?>
+    <?php echo wpautop( $data['content'] ); ?>
 </div>
