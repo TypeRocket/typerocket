721fc7f275e148832d355b8cf2477b5fb73156aa
Kevin Dees <kvdees@gmail.com>
2018-07-20 17:41:27 -0400

    add location field js

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,9 +29,11 @@ mix.js('resources/assets/js/admin.js', 'wordpress/assets/templates/js')
 
 // TypeRocket Core
 mix.js('vendor/typerocket/core/assets/js/core.js', 'wordpress/assets/typerocket/js/core.js')
+    .js('vendor/typerocket/core/assets/js/location.field.js', 'wordpress/assets/typerocket/js/location.field.js')
     .sass('vendor/typerocket/core/assets/sass/core.scss', 'wordpress/assets/typerocket/css/core.css')
     .copyDirectory('vendor/typerocket/core/assets/fonts', 'wordpress/assets/typerocket/fonts')
     .copyDirectory('vendor/typerocket/core/assets/js/lib', 'wordpress/assets/typerocket/js/lib')
+    .sourceMaps()
     .options({
         processCssUrls: false
     });
