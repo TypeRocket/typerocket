5c880bf353fb1b73baa5518c62246abcd151e361
Kevin Dees <kvdees@gmail.com>
2022-07-19 16:53:03 -0400

    add config/queue.php

---
+++ config/queue.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | TypeRocket Job Queue - Action Scheduler
+    |--------------------------------------------------------------------------
+    |
+    | The TypeRocket jobs queue system used the WooCommerce Action Scheduler.
+    | If you have a root installation of TypeRocket run the following:
+    |
+    | > composer require woocommerce/action-scheduler
+    |
+    | @link https://actionscheduler.org/
+    |
+    */
+    'action_scheduler' => [
+        'retention_period' => DAY_IN_SECONDS * 30,
+        'failure_period' => MINUTE_IN_SECONDS * 60,
+        'timeout_period' => MINUTE_IN_SECONDS * 10,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Jobs - Action Scheduler
+    |--------------------------------------------------------------------------
+    |
+    | Jobs must be registered for the Action Scheduler to detect and run
+    | your jobs.
+    |
+    */
+    'jobs' => [
+
+    ]
+];
