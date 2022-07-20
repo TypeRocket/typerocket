6a57038d99b1bbad48515a9e267abc946074cebe
Kevin Dees <kvdees@gmail.com>
2022-07-19 20:09:41 -0400

    add comments to config/queue.php

--- config/queue.php
+++ config/queue.php
@@ -14,8 +14,39 @@ return [
     |
     */
     'action_scheduler' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Retention Period - Action Scheduler
+        |--------------------------------------------------------------------------
+        |
+        | How long to keep actions with STATUS_COMPLETE and STATUS_CANCELED within
+        | the database after specified number of seconds. However, this does not
+        | clean up STATUS_FAILED actions.
+        |
+        */
         'retention_period' => DAY_IN_SECONDS * 30,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Failure Period - Action Scheduler
+        |--------------------------------------------------------------------------
+        |
+        | The number of seconds to allow an action to run before it is considered
+        | to have failed.
+        |
+        */
         'failure_period' => MINUTE_IN_SECONDS * 60,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Timeout Period - Action Scheduler
+        |--------------------------------------------------------------------------
+        |
+        | The number of seconds to allow a queue to run before unclaiming its
+        | pending actions. Actions remain pending and can be claimed again.
+        |
+        */
         'timeout_period' => MINUTE_IN_SECONDS * 10,
     ],
 
