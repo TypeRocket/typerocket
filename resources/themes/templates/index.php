0f3e597d451e2ac84cd26426c679b2840f03a03a
Kevin Dees <kvdees@gmail.com>
2022-07-27 13:52:25 -0400

    add view comment

--- resources/themes/templates/index.php
+++ resources/themes/templates/index.php
@@ -1,2 +1,13 @@
 <?php
+
+/*
+|--------------------------------------------------------------------------
+| Main View
+|--------------------------------------------------------------------------
+|
+| Here is where you can replace the standard WordPress templating system
+| with TypeRocket views loaded from the configured paths.views folder.
+|
+*/
+
 \TypeRocket\Template\View::new('master', ['title' => 'Andromeda'])->render();
