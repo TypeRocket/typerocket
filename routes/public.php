03f3f8a5ac2aad4da85e83816c47c0659cfe3ada
Kevin Dees <kvdees@gmail.com>
2022-07-27 12:35:18 -0400

    improve web routes public.php comments

--- routes/public.php
+++ routes/public.php
@@ -1,6 +1,12 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
-| Routes
+| Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your WordPress site. These
+| routes are loaded from the TypeRocket\Http\RouteCollection immediately
+| after the typerocket_routes action is fired.
+|
 */
