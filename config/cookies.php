a210cdd0bb5b27f7a0b16b244d6404d43fc1cc2d
Kevin Dees <kvdees@gmail.com>
2022-08-01 21:56:33 -0400

    remove x-frame-options from cookie system. Wrong location.

--- config/cookies.php
+++ config/cookies.php
@@ -24,17 +24,5 @@ return [
         |
         */
         'same_site' => 'Lax',
-
-        /*
-        |--------------------------------------------------------------------------
-        | Timeout Period - Action Scheduler
-        |--------------------------------------------------------------------------
-        |
-        | By default, WordPress adds `X-Frame-Options: SAMEORIGIN`. However, these
-        | headers are often set by the web server instead. Set this option as
-        | `false` to disable WordPress' x-frame-options.
-        |
-        */
-        'x_frame_options' => true,
     ]
 ];
