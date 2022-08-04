d24cb624ecf9ef4878ec5a20e699e7e8aed8ab88
Kevin Dees <kvdees@gmail.com>
2022-08-03 23:29:29 -0400

    add new database config for multi db connection system

---
+++ config/database.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Database Connection
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default database driver that is used when a query
+    | or model is instantiated. See the list of connections below for more
+    | details or add your own to the list. There is one by default.
+    |
+    */
+    'default' => typerocket_env('TYPEROCKET_DATABASE_DEFAULT', 'wp'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the database drivers for your application.
+    |
+    | Available Connections: "wordpress"
+    */
+    'drivers' => [
+        'wp' => [
+            'driver' => '\TypeRocket\Database\Connectors\WordPressCoreDatabaseConnector',
+        ],
+
+        'alt' => [
+            'driver' => '\TypeRocket\Database\Connectors\CoreDatabaseConnector',
+            'username' => '',
+            'password' => '',
+            'database' => '',
+            'host' => '',
+        ],
+    ]
+];
